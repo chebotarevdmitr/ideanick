40f6a56a548b3e3f4013a0b542bbb77e7d087fc2
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-02-09 18:07:29 +0300

    added tRPC for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,18 @@
 import express from 'express'
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { trpcRouter } from './trpc'
 
- const ideas = [
-    {
-      nick: "cool-idea-nick-1",
-      name: "idea 1",
-      description: "Description of idea 1...",
-    },
-    {
-      nick: "cool-idea-nick-2",
-      name: "idea 2",
-      description: "Description of idea 2...",
-    },
-    {
-      nick: "cool-idea-nick-3",
-      name: "idea 3",
-      description: "Description of idea 3...",
-    },
-    {
-      nick: "cool-idea-nick-4",
-      name: "idea 4",
-      description: "Description of idea 4...",
-    },
-    {
-      nick: "cool-idea-nick-5",
-      name: "idea 5",
-      description: "Description of idea 5...",
-    },
-  ];
 
 const expressApp = express()
 expressApp.get('/ping',(req,res) => {
   res.send('pong')
 })
-expressApp.get('/ideas',(req,res) => {
-  res.send(ideas)
-})
+
+
+expressApp.use('/tprc',trpcExpress.createExpressMiddleware({
+  router: trpcRouter,
+}))
+
 expressApp.listen(3000,() =>{
   console.info("Listenig at http://localhost:3000")
 } )
